66b26204671448208c133d4b445b4ccd8874894c
David Cole <david.cole1340@gmail.com>
2020-09-25 00:45:49 +1200

    tranzql: moved schema into graphql file, resolvers into ts file

--- TranzQL/index.ts
+++ TranzQL/index.ts
@@ -17,16 +17,27 @@ import express from "express"
 import {
   graphqlHTTP
 } from "express-graphql"
-import Schema from "./schema"
+import { buildSchema } from "graphql"
+import fs from "fs"
+import { MongoClient } from "mongodb";
+import getResolvers from "./resolvers"
+import { makeExecutableSchema } from "graphql-tools";
 
 const app = express();
+const uri = `mongodb://${MONGO_HOST}:27017`;
 
-Schema(MONGO_HOST, MONGO_DB).then((sch) => {
+MongoClient.connect(uri, { useUnifiedTopology: true }).then((client) => {
+  const db = client.db(MONGO_DB);
+  const typeDefs = fs.readFileSync('./schema.graphql').toString()
+  const resolvers = getResolvers(db)
+
   app.use('/graphql', graphqlHTTP({
     graphiql: process.env.NODE_ENV === 'development',
-    schema: sch
+    schema: makeExecutableSchema({
+      typeDefs,
+      resolvers
+    })
   }))
 
   app.listen(TRANZQL_PORT, () => console.log(`Listening on port ${TRANZQL_PORT}`))
-}).catch(console.error)
-
+})
---
+++ TranzQL/resolvers.ts
@@ -0,0 +1,60 @@
+import { IResolvers } from "graphql-tools";
+import { Db } from "mongodb";
+
+export default (db: Db): IResolvers => ({
+  Query: {
+    shifts: () => {
+      return db.collection('shifts').find({}).toArray()
+    },
+
+    shift: (parent, args) => {
+      return db.collection('shifts').find({
+        _id: args.id
+      }).toArray().then(shifts => shifts[0])
+    } 
+  },
+
+  Shift: {
+    splits: (shift) => {
+      return db.collection('splits').find({
+        shift_id: shift._id
+      }).toArray()
+    }
+  },
+
+  Split: {
+    shift: (split) => {
+      return db.collection('shifts').find({
+        _id: split.shift_id
+      }).toArray()
+    },
+
+    trips: (split) => {
+      return db.collection('trips').find({
+        split_id: split._id
+      }).toArray()
+    },
+
+    breaks: (split) => {
+      return db.collection('breaks').find({
+        split_id: split._id
+      }).toArray()
+    }
+  },
+
+  Trip: {
+    split: (trip) => {
+      return db.collection('splits').find({
+        _id: trip.split_id
+      }).toArray().then(trips => trips[0])
+    }
+  },
+
+  Break: {
+    split: (_break) => {
+      return db.collection('splits').find({
+        _id: _break.split_id
+      }).toArray().then(trips => trips[0])
+    }
+  }
+})
