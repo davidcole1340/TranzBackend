dc8605c804721bd589416dbed5d741f24451c909
David Cole <david.cole1340@gmail.com>
2021-12-05 17:23:33 +1300

    Fixed errors with graphql-tools

--- TranzQL/index.ts
+++ TranzQL/index.ts
@@ -14,7 +14,6 @@ const {
 } = process.env;
 
 import http from "http"
-import https from "https"
 import express from "express"
 import {
   graphqlHTTP
@@ -22,10 +21,10 @@ import {
 import fs from "fs"
 import { MongoClient } from "mongodb";
 import { tranzResolvers, gtfsResolvers, getBusData } from './resolvers'
-import {  makeExecutableSchema } from "graphql-tools";
 import merge from "lodash.merge"
 import { checkVersions } from "./watch";
 import { IResolvers } from "@graphql-tools/utils";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
 const app = express();
 const uri = `mongodb://${MONGO_HOST}:27017`;
@@ -35,51 +34,50 @@ const client = new MongoClient(uri);
 
 const main = async () => {
   await client.connect();
-  const tranzDb = client.db(MONGO_DB);
-  const gtfsDb = client.db(MONGO_GTFS_DB);
 
-  checkVersions(gtfsDb);
-  setInterval(() => checkVersions(gtfsDb), 1 * hour);
+  try {
+    const tranzDb = client.db(MONGO_DB);
+    const gtfsDb = client.db(MONGO_GTFS_DB);
 
-  const typeDefs: string = (() => {
-    const files = fs.readdirSync('./schema')
-    let content = '';
+    checkVersions(gtfsDb);
+    setInterval(() => checkVersions(gtfsDb), 1 * hour);
 
-    for (const file of files) {
-      content += fs.readFileSync('./schema/'+file).toString()
-    }
+    const typeDefs: string = (() => {
+      const files = fs.readdirSync('./schema')
+      let content = '';
 
-    return content;
-  })();
+      for (const file of files) {
+        content += fs.readFileSync('./schema/'+file).toString()
+      }
 
-  const resolvers: IResolvers = merge(tranzResolvers(tranzDb, gtfsDb), gtfsResolvers(tranzDb, gtfsDb));
+      return content;
+    })();
 
-  app.use('/graphql', graphqlHTTP({
-    graphiql: process.env.NODE_ENV === 'development',
-    schema: makeExecutableSchema({
-      typeDefs,
-      resolvers
-    })
-  }))
+    const resolvers: IResolvers = merge(tranzResolvers(tranzDb, gtfsDb), gtfsResolvers(tranzDb, gtfsDb));
 
-  app.get('/vehicles', (req, res) => {
-    getBusData().then(data => {
-      res.send(data);
-    }).catch((e: Error) => {
-      res.status(500).send({
-        error: e.message
+    app.use('/graphql', graphqlHTTP({
+      graphiql: process.env.NODE_ENV === 'development',
+      schema: makeExecutableSchema({
+        typeDefs,
+        resolvers
+      })
+    }))
+
+    app.get('/vehicles', (req, res) => {
+      getBusData().then(data => {
+        res.send(data);
+      }).catch((e: Error) => {
+        res.status(500).send({
+          error: e.message
+        });
       });
     });
-  });
 
-  const httpServer = http.createServer(app);
-  httpServer.listen(8080, () => console.log(`Listening on port 8080`));
+    const httpServer = http.createServer(app);
+    httpServer.listen(8080, () => console.log(`Listening on port 8080`));
+  } finally {
+    await client.close()
+  }
 }
 
-try {
-  main()
-} catch (e) {
-  console.dir(e)
-} finally {
-  client.close()
-}
+main().catch(console.dir)
